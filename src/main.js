710ae485d510172c9f295962bdd831da51257587
Orange <635000035@qq.com>
2021-04-21 10:35:34 +0800

    页面及功能调整

--- src/main.js
+++ src/main.js
@@ -11,9 +11,9 @@ Vue.use(ElementUI)
 
 Vue.use(uploader)
 
-Vue.prototype.$IMGURL = 'http://47.112.217.146:90' //统一设置页面上图片URL路径入口（前台图片）
+Vue.prototype.$IMGURL = 'http://www.amzbuy.cn' //统一设置页面上图片URL路径入口（前台图片）
 
-Vue.prototype.$IMGURLBACK = 'http://47.112.217.146:9090' //统一设置页面上图片URL路径入口（后台图片，列表导出图片需要用到完整路径）
+Vue.prototype.$IMGURLBACK = 'http://admin.amzbuy.cn' //统一设置页面上图片URL路径入口（后台图片，列表导出图片需要用到完整路径）
 
 router.beforeEach((to, from, next) => {
 	let userId = sessionStorage.getItem('userId')
--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
 		proxy: {
 			//设置代理
 			'/api': {
-				target: 'http://47.112.217.146:9090',
+				target: 'http://admin.amzbuy.cn',
 				changeOrigin: true,
 				pathRewrite: {
 					'^/api': '/api'
